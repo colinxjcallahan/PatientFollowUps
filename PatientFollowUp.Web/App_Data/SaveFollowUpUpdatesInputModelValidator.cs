4c3d3a6405aebcb89ae1be58cb7cb29a0250ac70
colinxjcallahan <colin@acklenavenue.com>
2014-08-03 08:38:04 -0700

    Hmm, don't remember

--- PatientFollowUp.Web/App_Data/SaveFollowUpUpdatesInputModelValidator.cs
+++ PatientFollowUp.Web/App_Data/SaveFollowUpUpdatesInputModelValidator.cs
@@ -3,31 +3,28 @@ using PatientFollowUp.Web.Models;
 
 namespace PatientFollowUp.Web.App_Data
 {
-    //public class SaveFollowUpUpdatesInputModelValidator : AbstractValidator<SaveFollowUpUpdatesInputModel>, IValidator
-    //{
-    //    public new ValidationResult Validate(SaveFollowUpUpdatesInputModel saveFollowUpUpdatesInputModel)
-    //    {
-    //        var fluentValidationResult = base.Validate(saveFollowUpUpdatesInputModel);
-    //        var validationResult = new ValidationResult();
-    //        if (!fluentValidationResult.IsValid)
-    //        {
-    //            foreach (var validationFailure in fluentValidationResult.Errors)
-    //            {
-    //                validationResult.AddError(validationFailure.ErrorMessage);
-    //            }
-    //        }
-    //        return validationResult;
-    //    }
-    //}
-
-    public class SaveFollowUpUpdatesInputModelValidator : IValidator
+    public class SaveFollowUpUpdatesInputModelValidator : AbstractValidator<SaveFollowUpUpdatesInputModel>, IValidator
     {
+        public SaveFollowUpUpdatesInputModelValidator()
+        {
+            RuleFor(x => x.FollowUpExamId)
+                .NotNull()
+                .GreaterThan(0)
+                .When(x => x.NoRelevantFollowUpFound == false)
+                .WithMessage("Either select an exam ID or select 'No Relevant Followup' and add a comment");
+        }
         public new ValidationResult Validate(SaveFollowUpUpdatesInputModel saveFollowUpUpdatesInputModel)
         {
-            return new ValidationResult
+            var fluentValidationResult = base.Validate(saveFollowUpUpdatesInputModel);
+            var validationResult = new ValidationResult();
+            if (!fluentValidationResult.IsValid)
             {
-                IsValid = true,
-            };
+                foreach (var validationFailure in fluentValidationResult.Errors)
+                {
+                    validationResult.AddError(validationFailure.ErrorMessage);
+                }
+            }
+            return validationResult;
         }
     }
 }
